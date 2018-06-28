d203c7f53a13a0d419e81ae7c0105d2aaa74ee1e
Duong Nguyen <daiduong17@gmail.com>
2018-06-29 00:10:53 +0700

    first draft

---
+++ iPremium.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using iPremium.Controls;
+using iPremium.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace iPremium.iOS.Renderers
+{
+    public class CustomEntryRenderer: EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.TextAlignment = UITextAlignment.Center;
+            Control.Layer.BorderColor = UIColor.White.CGColor;
+            Control.Layer.BorderWidth = 1;
+            Control.Layer.CornerRadius = 5;
+        }
+    }
+}
---
+++ iPremium/Controls/CustomEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace iPremium.Controls
+{
+    public class CustomEntry: Entry
+    {
+        public CustomEntry()
+        {
+        }
+    }
+}
